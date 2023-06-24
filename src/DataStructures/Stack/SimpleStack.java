841032b1ac344397b7ef45dfd529d68bc3f6f315
MaksymilianSt <Maksymilianst02@gmail.com>
2023-06-24 21:03:29 +0200

    SimpleStack class created

---
+++ src/DataStructures/Stack/SimpleStack.java
@@ -0,0 +1,32 @@
+package DataStructures.Stack;
+
+import java.util.Arrays;
+
+public class SimpleStack <T> {
+    private Object [] stack = new Object [10];
+    private int top = 0;
+    public void push(T element){
+        stack[top++] = element;
+        if(top == stack.length)
+            doubleStackArray();
+    }
+    public T pop(){
+        if (top == 0) throw new ArrayIndexOutOfBoundsException("stack is empty");
+        return (T) stack[(top--) -1];
+    }
+    public T peek(){
+        if (top == 0) throw new ArrayIndexOutOfBoundsException("stack is empty");
+        return (T) stack[top -1];
+    }
+    public int size(){
+        return top;
+    }
+    private void doubleStackArray(){
+       stack = Arrays.copyOf(stack,stack.length * 2);
+    }
+    public void show(){
+        Arrays.stream(stack).forEach(System.out::println);
+    }
+
+
+}
