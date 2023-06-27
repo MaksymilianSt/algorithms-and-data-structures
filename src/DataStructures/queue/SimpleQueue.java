43dba13cfbff610e4f8cc5134fc180ffeb580f47
MaksymilianSt <Maksymilianst02@gmail.com>
2023-06-27 18:13:29 +0200

    Queue methods implemented

--- src/DataStructures/queue/SimpleQueue.java
+++ src/DataStructures/queue/SimpleQueue.java
@@ -1,4 +1,25 @@
 package DataStructures.queue;
 
-public class SimpleQueue {
+import java.util.Arrays;
+
+public class SimpleQueue <T> {
+   private Object [] queue = new Object[5];
+   private int size;
+   private int front;
+   private int rear;
+    public void enQueue(T data){
+        if(size == queue.length)
+            doubleQueueSize();
+        queue[rear++] = data;
+        size++;
+
+    }
+    public T deQueue(){
+        T data = (T) queue[front++];
+        size--;
+        return data;
+    }
+    private void doubleQueueSize(){
+       queue =  Arrays.copyOf(queue ,queue.length*2);
+    }
 }
