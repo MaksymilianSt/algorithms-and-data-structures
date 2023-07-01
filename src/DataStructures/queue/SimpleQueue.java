69d19468d91f530d58cfddf435f40c505e1b47a9
MaksymilianSt <Maksymilianst02@gmail.com>
2023-07-01 16:41:43 +0200

    Queue methods implemented

--- src/DataStructures/queue/SimpleQueue.java
+++ src/DataStructures/queue/SimpleQueue.java
@@ -2,31 +2,41 @@ package DataStructures.queue;
 
 import java.util.Arrays;
 
-public class SimpleQueue <T> {
-   private Object [] queue = new Object[5];
-   private int size;
-   private int front;
-   private int rear;
-    public void enQueue(T data){
-        if(size == queue.length)
+public class SimpleQueue<T> {
+    private Object[] queue = new Object[5];
+    private int size;
+    private int front;
+    private int rear;
+
+    public void enQueue(T data) {
+        if (size == queue.length)
             doubleQueueSize();
         queue[rear++] = data;
         size++;
 
     }
-    public T deQueue(){
-        if(size <= 0)
+
+    public T deQueue() {
+        if (size <= 0)
             throw new IndexOutOfBoundsException("there are no more elements ");
         T data = (T) queue[front++];
         size--;
         return data;
     }
-    public void info(){
-        for (int i = front; i <rear ; i++) {
+
+    public T peek() {
+        if (size <= 0)
+            throw new IndexOutOfBoundsException("there are no more elements ");
+        return (T) queue[front];
+    }
+
+    public void info() {
+        for (int i = front; i < rear; i++) {
             System.out.print(queue[i] + " , ");
         }
     }
-    private void doubleQueueSize(){
-       queue =  Arrays.copyOf(queue ,queue.length*2);
+
+    private void doubleQueueSize() {
+        queue = Arrays.copyOf(queue, queue.length * 2);
     }
 }
