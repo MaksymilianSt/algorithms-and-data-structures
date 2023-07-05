d54a954508a710da2a84c8c1017f54fd245ce1cb
MaksymilianSt <Maksymilianst02@gmail.com>
2023-07-05 08:00:32 +0200

    partition method created

--- src/Algorithms/Sort/QuickSort.java
+++ src/Algorithms/Sort/QuickSort.java
@@ -6,7 +6,40 @@ import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort {
-    public static void main(String[] args) {
+    private static int partition(int array[], int low, int high) {
+
+        // choose the rightmost element as pivot
+        int pivot = array[high];
+
+        // pointer for greater element
+        int i = (low - 1);
+
+        // traverse through all elements
+        // compare each element with pivot
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+
+                // if element smaller than pivot is found
+                // swap it with the greater element pointed by i
+                i++;
+
+                // swapping element at i with element at j
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+
+        }
+
+        // swapt the pivot element with the greater element specified by i
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        // return the position from where partition is done
+        return (i + 1);
+    }
+    public static void main(String[] args)  {
 
     }
 }
